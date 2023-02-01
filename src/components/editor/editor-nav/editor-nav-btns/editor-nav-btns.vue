4f4da4dcddadd7ec3f39647013d5d4e30a39d7f3
Denis Lit <denislitviak@gmail.com>
2023-02-01 21:58:55 +0200

    add back btn cmp to split code, add v-for on the editor-nav-btns

--- src/components/editor/editor-nav/editor-nav-btns/editor-nav-btns.vue
+++ src/components/editor/editor-nav/editor-nav-btns/editor-nav-btns.vue
@@ -1,9 +1,34 @@
 <script setup lang="ts">
 import aTooltip from '@/material/a-tooltip.vue'
 import { eventBus } from '@/services/event-bus.service'
+import backBtn from '@/material/back-btn.vue'
 import { ref } from 'vue'
 
 const viewMode = ref(1700)
+const btns = ref([
+    {
+        id: 'btn-101',
+        content: 'Switch to desktop',
+        icon: 'fa-light fa-display',
+        class: { active: viewMode.value === 1700 },
+        size: 1700,
+    },
+    {
+        id: 'btn-102',
+        content: 'Switch to tablet',
+        icon: 'fa-light fa-tablet-screen',
+        class: { active: viewMode.value === 800 },
+        size: 800,
+    },
+    {
+        id: 'btn-103',
+        content: 'Switch to mobile',
+        icon: 'fa-light fa-mobile',
+        class: { active: viewMode.value === 420 },
+        size: 420,
+    }
+]
+)
 const resize = (size: number) => {
     viewMode.value = size
     eventBus.emit('resized', size)
@@ -11,38 +36,24 @@ const resize = (size: number) => {
 </script>
 
 <template>
-    <div class="back">
-        <a-tooltip placement="bottom" content="Go back">
-            <template #payload>
-                <router-link class="back-link" to="/template">
-                    <font-awesome-icon icon="fa-light fa-arrow-left" />
-                </router-link>
-            </template>
-        </a-tooltip>
-    </div>
-
+    <back-btn to="/template" content="Go Back"/>
     <div class="view-port flex items-center">
-        <a-tooltip @click="resize(1700)" placement="bottom" content="Switch to desktop">
-            <template #payload>
-                <div>
-                    <font-awesome-icon icon="fa-light fa-display" :class="{ active: viewMode === 1700 }" class="icon" />
-                </div>
-            </template>
-        </a-tooltip>
-        <a-tooltip @click="resize(800)" placement="bottom" content="Switch to tablet">
-            <template #payload>
-                <div>
-                    <font-awesome-icon icon="fa-light fa-tablet-screen" :class="{ active: viewMode === 800 }"
-                        class="icon" />
-                </div>
-            </template>
-        </a-tooltip>
-        <a-tooltip @click="resize(420)" placement="bottom" content="Switch to mobile">
-            <template #payload>
-                <div>
-                    <font-awesome-icon icon="fa-light fa-mobile" :class="{ active: viewMode === 420 }" class="icon" />
-                </div>
-            </template>
-        </a-tooltip>
+        <template v-for="btn in btns">
+            <a-tooltip 
+                @click="resize(btn.size)" 
+                placement="bottom" 
+                :content="btn.content"
+            >
+                <template #payload>
+                    <div>
+                        <font-awesome-icon 
+                            :icon="btn.icon" 
+                            :class="btn.class" 
+                            class="icon" 
+                        />
+                    </div>
+                </template>
+            </a-tooltip>
+        </template>
     </div>
 </template>
---
+++ src/material/back-btn.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+const props = defineProps({
+    to: String,
+    content: String
+})
+</script>
+
+<template>
+    <div class="back">
+        <a-tooltip placement="bottom" :content="content">
+            <template #payload>
+                <router-link class="back-link" :to="to || '/'">
+                    <font-awesome-icon icon="fa-light fa-arrow-left" />
+                </router-link>
+            </template>
+        </a-tooltip>
+    </div>
+</template>
