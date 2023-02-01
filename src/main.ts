76927523d8c88b25c86e398599ccb091eeb6d79d
Denis Lit <denislitviak@gmail.com>
2023-02-01 22:25:34 +0200

    add fa icon library, icons imports plugin

--- src/main.ts
+++ src/main.ts
@@ -5,8 +5,16 @@ import router from './router'
 import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
 import './styles.scss'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import { icons } from '@/plugins/fa-icons'
 
 const app = createApp(App)
+library.add(...icons)
+app.component(
+    'font-awesome-icon',
+    FontAwesomeIcon
+)
 app.use(router)
 app.use(store)
 app.use(ElementPlus)
--- src/material/back-btn.vue
+++ src/material/back-btn.vue
@@ -1,4 +1,5 @@
 <script setup lang="ts">
+import aTooltip from './a-tooltip.vue'
 const props = defineProps({
     to: String,
     content: String
